5cca05b0b2b8b8e13f6192fcd1a07c44efec380e
Duke <duke@riseup.net>
2012-09-12 12:10:55 -0300

    foi have many comments

--- app/models/foi.rb
+++ app/models/foi.rb
@@ -1,3 +1,5 @@
 class Foi < ActiveRecord::Base
   attr_accessible :description, :observation, :org
+
+  has_many :comments
 end
---
+++ spec/models/foi_spec.rb
@@ -0,0 +1,5 @@
+require 'spec_helper'
+
+describe Foi do
+  it { should have_many(:comments) }
+end
