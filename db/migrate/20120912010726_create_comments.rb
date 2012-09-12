5f7a2c66dd9432b1505b0bd90c62338ffbbe2330
Duke <duke@riseup.net>
2012-09-12 12:10:35 -0300

    fixed comments migrate

--- db/migrate/20120912010726_create_comments.rb
+++ db/migrate/20120912010726_create_comments.rb
@@ -2,7 +2,7 @@ class CreateComments < ActiveRecord::Migration
   def change
     create_table :comments do |t|
       t.text :body
-      t.reference :foi
+      t.references :foi
 
       t.timestamps
     end
