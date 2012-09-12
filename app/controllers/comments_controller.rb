a71cf7141ccc36079d1912881869f08ad52852ff
Duke <duke@riseup.net>
2012-09-12 12:08:55 -0300

    fixed comments redirect and save foi to comment

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -3,14 +3,13 @@ class CommentsController < ApplicationController
   # POST /comments.json
   def create
     @comment = Comment.new(params[:comment])
+    @comment.update_attribute :foi_id, params[:foi_id]
 
     respond_to do |format|
       if @comment.save
-        format.html { redirect_to @comment, notice: 'Comment was successfully created.' }
-        format.json { render json: @comment, status: :created, location: @comment }
+        format.html { redirect_to @comment.foi, notice: 'Comment was successfully created.' }
       else
-        format.html { render action: "new" }
-        format.json { render json: @comment.errors, status: :unprocessable_entity }
+        format.html { redirect_to @comment.foi, notice: 'Comment wasnt created' }
       end
     end
   end
