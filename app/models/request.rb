4c7e01273345aeb864bd4298894cd006a17e8e65
Duke <duke@riseup.net>
2012-09-22 20:21:02 -0300

    includes comments to request

--- app/models/request.rb
+++ app/models/request.rb
@@ -1,6 +1,6 @@
 class Request < ActiveRecord::Base
   attr_accessible :description, :observation, :resume
-  default_scope order("id DESC")
+  default_scope order("id DESC").includes(:comments)
   has_many :comments
   has_many :adopts
 end
