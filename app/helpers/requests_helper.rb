86d20ae7cd027039d1e014ae59327d0fc03bbb45
Duke <duke@riseup.net>
2012-09-23 10:39:16 -0300

    changed pluralize helper to count

--- app/helpers/requests_helper.rb
+++ app/helpers/requests_helper.rb
@@ -3,7 +3,15 @@ module RequestsHelper
 
   alias_method :old_pluralize, :pluralize
 
-  def pluralize(count, singular, plural = nil, zero = nil)
+  def count(model)
+    count = model.count.to_i
+    model_name = model.model_name
+    model_name_plural = model_name.plural
+
+    singular = t(:"#{model_name.singular}", scope: ["pluralize", :"#{model_name_plural}"])
+    plural = t(:"#{model_name_plural}", scope: ["pluralize", :"#{model_name_plural}"])
+    zero = t(:"no#{model_name_plural}", scope: ["pluralize", :"#{model_name_plural}"])
+
     return zero if count == 0
     old_pluralize count, singular, plural
   end
