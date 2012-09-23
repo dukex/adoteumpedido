6c38d7c39b6d79bc3bed2424563167673105c668
Duke <duke@riseup.net>
2012-09-23 13:23:12 -0300

    send rails cache to /dev/null

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -37,7 +37,7 @@ AdoteSeuPedido::Application.configure do
   # config.log_tags = [ :subdomain, :uuid ]
 
   # Use a different logger for distributed setups
-  # config.logger = ActiveSupport::TaggedLogging.new(SyslogLogger.new)
+  config.logger = Logger.new('/dev/null')
 
   # Use a different cache store in production
   # config.cache_store = :mem_cache_store
