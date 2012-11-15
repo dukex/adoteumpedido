39aadd48e9e3e03cb0b09b54ba4d144637e102b5
Duke <duke@vertigem.xxx>
2012-11-15 04:08:38 -0200

    restarting unicorn after deploy

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,3 +6,7 @@ hosts = ["173.255.218.86"]
 before_restarting_server do
   rake "assets:precompile"
 end
+
+after_restarting_server do
+  run "kill -s USR2 `cat /var/local/apps/adoteumpedido/tmp/pids/unicorn.pid`"
+end
