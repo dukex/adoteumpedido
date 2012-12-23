41115e1ad72e41f3760b840b3c5f01e09ecd7cf1
Duke <duke@vertigem.xxx>
2012-12-23 19:46:24 -0200

    installed active admin

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 AdoteSeuPedido::Application.routes.draw do
+  ActiveAdmin.routes(self)
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+
   get "authorities" => "authority#index"
 
   devise_for :users
