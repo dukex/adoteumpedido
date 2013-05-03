a97a2f9c28afcf88ddf9dfe93861e3a7da84c1cb
Duke <duke@vertigem.xxx>
2013-05-03 11:15:38 -0300

    added title helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  def title
+    title = [t(:name, scope: [:application])]
+    if content_for?(:title)
+      title <<"|"
+      title << content_for(:title)
+    end
+    title.join(" ")
+  end
 end
